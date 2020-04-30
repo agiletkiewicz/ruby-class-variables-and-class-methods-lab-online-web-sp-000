12fb5dbcf6077928a4d189c6fb23f2e507ee5c0e
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-04-30 00:08:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,7 +17,14 @@ def Song
     @@count 
   end
 
-  
+  def self.genres 
+    unique_genre_array = []
+    @@genres.each do |genre|
+      if !unique_genre_array.include?(genre)
+        unique_genre_array << genre
+      end
+    end
+  end
   
   
 end
